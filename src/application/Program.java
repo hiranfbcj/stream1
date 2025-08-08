ac3c95bece6c7e864709e3d9025c6a7554525587
Hiran Ferreti Baccos <hiranfbcj@outlook.com>
2025-08-08 20:39:45 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+
+		Stream<Integer> st1 = list.stream();
+		System.out.println(Arrays.toString(st1.toArray()));
+
+		Stream<Integer> st2 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st2.toArray()));
+
+		Stream<String> st3 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st3.toArray()));
+
+		Stream<Integer> st4 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
+		Stream<Long> st5 = Stream.iterate(new Long[] { 0L, 1L }, p -> new Long[] { p[1], p[0] + p[1] }).map(p -> p[0]); // Sequência de Fibonacci
+		System.out.println(Arrays.toString(st5.limit(10).toArray()));
+	}
+}
